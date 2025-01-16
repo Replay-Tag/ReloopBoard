a6f107f6b2013c8a1582d8e1ef03a72ed64aebd7
replay tag <97656665+Replay-Tag@users.noreply.github.com>
2025-01-16 15:52:14 -0500

    Add files via upload

---
+++ src/ReloopBoard/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+using System.Diagnostics;
+using ReloopBoard.Pages;
+
+namespace ReloopBoard
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+
+        private void SoundsButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Sounds soundsWindow = new Sounds();
+            soundsWindow.Show();
+
+            
+            this.Hide();
+        }
+
+        
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            this.Hide();
+            MainWindow homeWindow = new MainWindow();
+            homeWindow.Show();
+        }
+
+        
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            Settings settingsWindow = new Settings();
+            settingsWindow.Show();
+            this.Close();
+        }
+
+        
+        private void GitHubButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            string url = "https://github.com/Replay-Tag/ReloopBoard";
+            Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+        }
+    }
+}
